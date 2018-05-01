0421e4f3b9846a5d396784f9249bffcd2cfe6ca6
lixu <1418807001@qq.com>
2018-05-01 17:04:52 +0800

    5.1 code update1

---
+++ IdeaTest1/src/main/java/com/lixu/test/IdeaController.java
@@ -0,0 +1,20 @@
+package com.lixu.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Hi_Lee on 2018/5/1.
+ */
+@Controller
+public class IdeaController {
+
+    @RequestMapping("hello")
+    @ResponseBody
+    public  String sayHello(){
+        return "hello" + System.currentTimeMillis();
+    }
+
+
+}
---
+++ IdeaTest1/src/main/java/com/lixu/test/IdeaTestApplication.java
@@ -0,0 +1,12 @@
+package com.lixu.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaTestApplication.class, args);
+	}
+}
